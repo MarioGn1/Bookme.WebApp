4dc3436683b4e6c14cfd7946f527c1a5320f7ed5
Mario <mariogn@mail.bg>
2021-07-16 18:18:32 +0300

    Allow Null values for OfferedService duration

--- Bookme.WebApp/Bookme.Data/Models/OfferedService.cs
+++ Bookme.WebApp/Bookme.Data/Models/OfferedService.cs
@@ -19,7 +19,7 @@ namespace Bookme.Data.Models
         [MaxLength(200)]
         public string Description { get; set; }
         public decimal Price { get; set; }
-        public int Duration { get; set; }
+        public int? Duration { get; set; }
         [Required]
         public int ServiceCategoryId { get; set; }
         public ServiceCategorie ServiceCategory { get; set; }
