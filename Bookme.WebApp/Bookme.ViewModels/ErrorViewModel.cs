1b775310a1075c2c26f1d14cc4dfeb738239fbac
Mario <mariogn@mail.bg>
2021-07-19 16:11:05 +0300

    Add a View Models layer

---
+++ Bookme.WebApp/Bookme.ViewModels/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Bookme.ViewModels
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Bookme.WebApp/Bookme.ViewModels/OfferedServices/ServiceCategoryViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Bookme.ViewModels.OfferedServices
+{
+    public class ServiceCategoryViewModel
+    {
+        [Required]
+        public string Name { get; set; }
+    }
+}
