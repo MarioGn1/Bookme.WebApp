6c382d85c8dcbc6b84ce368426147386105887df
Mario <mariogn@mail.bg>
2021-08-07 19:46:42 +0300

    Established Test project with testing home controller

---
+++ Bookme.WebApp/Bookme.Test/Controllers/HomeControllerTest.cs
@@ -0,0 +1,40 @@
+﻿using Xunit;
+using MyTested.AspNetCore.Mvc;
+using Bookme.WebApp.Controllers;
+
+using static Bookme.Test.Data.Category;
+using Bookme.ViewModels.HomeModels;
+using System.Collections.Generic;
+using Bookme.Data.Models;
+
+namespace Bookme.Test.Controllers
+{
+    public class HomeControllerTest
+    {
+        [Fact]
+        public void GetIndexShouldReturnViewWithoutUser()
+            => MyController<HomeController>
+                .Instance()
+                .Calling(c => c.Index())
+                .ShouldReturn()
+                .View();
+
+        [Fact]
+        public void GetIndexShouldReturnViewWithUser()
+            => MyController<HomeController>
+                .Instance()
+                .WithUser(TestUser.Identifier, "Mario", "Client")
+                .Calling(c => c.Index())            
+                .ShouldReturn()
+                .View(view => view
+                .WithModelOfType<IEnumerable<ClientHomeViewModel>>());
+
+        [Fact]
+        public void ErrorShouldReturnView()
+            => MyController<HomeController>
+                .Instance()
+                .Calling(c => c.Error())
+                .ShouldReturn()
+                .View();
+    }
+}
---
+++ Bookme.WebApp/Bookme.Test/Data/Category.cs
@@ -0,0 +1,12 @@
+﻿using Bookme.Data.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Bookme.Test.Data
+{
+    public static class Category
+    {
+        public static IEnumerable<ServiceCategory> SixCategories
+            => Enumerable.Range(0, 10).Select(c => new ServiceCategory());
+    }
+}
