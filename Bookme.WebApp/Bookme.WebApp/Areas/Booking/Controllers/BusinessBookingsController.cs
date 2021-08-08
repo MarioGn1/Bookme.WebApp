9d2c8e44b8e1a379ad428879a3fded787501f707
Mario <mariogn@mail.bg>
2021-08-08 15:03:37 +0300

    Add My Bookings and Business Bookings controllers and views

---
+++ Bookme.WebApp/Bookme.WebApp/Areas/Booking/Controllers/BusinessBookingsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bookme.WebApp.Areas.Booking.Controllers
+{
+    [Area("Booking")]
+    public class BusinessBookingsController : Controller
+    {
+        [Authorize]
+        public IActionResult All()
+        {
+            return View();
+        }
+    }
+}
