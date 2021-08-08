c074e89cf226aead02b402903c156f5fdb586dbc
Mario <mariogn@mail.bg>
2021-08-08 15:02:33 +0300

    Add routing tests for Home controller

---
+++ Bookme.WebApp/Bookme.Test/Routes/HomeControllerTest.cs
@@ -0,0 +1,23 @@
+﻿using Bookme.WebApp.Controllers;
+using MyTested.AspNetCore.Mvc;
+using Xunit;
+
+namespace Bookme.Test.Routes
+{
+    public class HomeControllerTest
+    {
+        [Fact]
+        public void GetIndexRouteShouldBeMapped()
+            => MyRouting
+                .Configuration()
+                .ShouldMap("/")
+                .To<HomeController>(c => c.Index());
+
+        [Fact]
+        public void GetErrorRouteShouldBeMapped()
+            => MyRouting
+                .Configuration()
+                .ShouldMap("/Home/Error")
+                .To<HomeController>(c => c.Error());
+    }
+}
